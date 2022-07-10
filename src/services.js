9711ce757d17b9462441e155862a71f742f29265
mateus <josemateus.ufma@gmail.com>
2022-07-10 16:25:50 -0300

    Estilo da home

---
+++ src/services.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:3000"
+})
+export const api = {
+    get(url){
+        return axiosInstance.get(url);
+    },
+    post(url, body) {
+        return axiosInstance.get(url, body);
+    }
+}
